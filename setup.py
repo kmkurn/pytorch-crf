6a6cc0166f4a8961c9d2f49652a5ae02a44afc59
Kemal Maulana <kemal@kkurniawan.com>
2017-11-25 12:51:46 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
 
+with open('README.md') as f:
+    readme = f.read()
+
+
 setup(name='pytorch-crf',
       version='0.1.0',
       description='Conditional random field in PyTorch',
+      long_description=readme,
       url='https://github.com/kmkurn/pytorch-crf',
       author='Kemal Kurniawan',
       author_email='kemal@kkurniawan.com',
