525297ed76398e3322bbf7d5e4d7cb3a5891f4f1
Kemal Kurniawan <kemal@kkurniawan.com>
2018-12-24 15:08:09 +0700

    Bump to v0.7.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='pytorch-crf',
-    version='0.6.0',
+    version='0.7.0',
     description='Conditional random field in PyTorch',
     long_description=readme,
     url='https://github.com/kmkurn/pytorch-crf',
