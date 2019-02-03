ba84c2672499d85ff8594f7e1c1f167decb8f38d
Kemal Kurniawan <kemal@kkurniawan.com>
2019-02-04 06:10:41 +0700

    Link to Python 3.6 docs

--- docs/conf.py
+++ docs/conf.py
@@ -178,6 +178,6 @@ epub_exclude_files = ['search.html']
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    'https://docs.python.org/': None,
+    'https://docs.python.org/3.6/': None,
     'https://pytorch.org/docs/stable/': None,
 }
