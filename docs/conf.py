ec54dc1b530caa647f8f04f1b182a4edc7d3af38
Kemal Kurniawan <kemal@kkurniawan.com>
2019-02-04 05:44:47 +0700

    Use RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,7 @@ project = 'pytorch-crf'
 copyright = '2019, Kemal Kurniawan'
 author = 'Kemal Kurniawan'
 
+# TODO single source version
 # The short X.Y version
 version = '0.7'
 # The full version, including alpha/beta/rc tags
@@ -80,7 +81,8 @@ default_role = 'py:obj'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
